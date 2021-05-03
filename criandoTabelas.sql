c1c3e79d58851df22e57d57938d4a741e15847c6
Ana Flávia <pierrout77@gmail.com>
2021-05-03 18:09:11 -0300

    Create criandoTabelas.sql

---
+++ criandoTabelas.sql
@@ -0,0 +1,27 @@
+```
+CREATE TABLE DVD (
+número int,
+ID smallint
+);
+
+CREATE TABLE filme (
+título varchar(20),
+categoria varchar(20),
+ID smallint PRIMARY KEY
+);
+
+CREATE TABLE cliente (
+sobrenome varchar(20),
+nome varchar(20),
+telefone int,
+enedereco varchar(20),
+num_associado smallint PRIMARY KEY
+);
+
+CREATE TABLE ator (
+nome varchar(20),
+data_nascimento date
+);
+
+ALTER TABLE DVD ADD FOREIGN KEY(ID) REFERENCES filme (ID)
+```
