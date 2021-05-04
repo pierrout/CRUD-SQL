daa3e469e73251d05e7d2741e146ff158f212249
Ana Flávia <pierrout77@gmail.com>
2021-05-04 12:05:53 -0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -1,8 +1,8 @@
-``` 
+ 
 insert into filme values ("A Lista de Schindler", "Drama",1);
 insert into filme values ("Um sonho de Liberdade", "Drama",2);
 insert into filme values ("Coraline", "Infantil",3);
 insert into filme values ("Harriet", "Drama",4);
 insert into filme values ("O menino do pijama listrado", "Drama",5);
 insert into filme values ("A noiva cadáver", "Infantil",6);
-```
+
