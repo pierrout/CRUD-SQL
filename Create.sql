5efc8c2229c28a7923bf80569915e3e368633f76
Ana Flávia <pierrout77@gmail.com>
2021-08-31 16:59:03 -0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -6,4 +6,8 @@ insert into filme values ("Harriet", "Drama",4);
 insert into filme values ("O menino do pijama listrado", "Drama",5);
 insert into filme values ("A noiva cadáver", "Infantil",6);
 
-
+insert into DVD values (234,1);
+insert into DVD values (235,2);
+insert into DVD values (236,3);
+insert into DVD values (237,4);
+insert into DVD values (238,5);
